d4c7b6b6c9cb352a699222affafbe0ddad94af20
diversen <dennis.iversen@gmail.com>
2011-03-30 22:19:56 +0200

    class reference in sql. Know what module to use.

--- mysql/down/1.03.sql
+++ mysql/down/1.03.sql
@@ -1,3 +1,5 @@
+ALTER TABLE `files` DROP INDEX `reference_index`;
+
 ALTER TABLE `files` DROP COLUMN `reference`;
 
 RENAME TABLE `files` TO `content_file`;
--- mysql/up/1.03.sql
+++ mysql/up/1.03.sql
@@ -1,3 +1,5 @@
 RENAME TABLE `content_file` TO `files`;
 
 ALTER TABLE `files` ADD `reference` varchar(255) NOT NULL;
+
+CREATE INDEX `reference_index` ON `files` (reference);
