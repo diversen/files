c45ff92dd3128835f995e09a081c0cfd2e06adfd
Dennis Iversen <dennis.iversen@gmail.com>
2011-12-18 19:12:03 +0100

    content length header

--- download.php
+++ download.php
@@ -12,5 +12,8 @@ files::setFileId($frag = 2);
 $file = $content_file->getFile();
 
 header("Content-type: $file[mimetype]");
+$data_len = strlen ($row['file']);
+
+header ("Content-Length: $data_len");
 echo $file['file'];
 die;
