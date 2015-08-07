deefea8a3fe211d7661c27dd348f21ade2e98f0b
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-07 18:36:20 +0200

    refactor lang

---
+++ lang/en/language.php
@@ -0,0 +1,20 @@
+<?php
+
+$LANG = array();
+
+// Translation of file modules/files/module.php
+
+$LANG['Add file'] = 'Add file';
+$LANG['Edit file'] = 'Edit file';
+$LANG['Delete file'] = 'Delete file';
+$LANG['Delete'] = 'Delete';
+$LANG['Update'] = 'Update';
+$LANG['Add'] = 'Add';
+$LANG['Abstract'] = 'Abstract';
+$LANG['No file was specified'] = 'No file was specified';
+$LANG['Edit files'] = 'Edit files';
+$LANG['Download'] = 'Download';
+$LANG['Edit'] = 'Edit';
+$LANG['File was added'] = 'File was added';
+$LANG['File was deleted'] = 'File was deleted';
+$LANG['File was edited'] = 'File was edited';
