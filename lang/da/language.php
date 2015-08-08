f5c92a2cd1f0dce4cf88211d4c9c20a3e357c2d0
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-08 15:17:03 +0200

    lang

---
+++ lang/da/language.php
@@ -0,0 +1,18 @@
+<?php
+
+$LANG = array();
+
+$LANG['Add file'] = 'Tilføj fil';
+$LANG['Edit file'] = 'Rediger fil';
+$LANG['Delete file'] = 'Slet fil';
+$LANG['Delete'] = 'Slet';
+$LANG['Update'] = 'Opdatering';
+$LANG['Add'] = 'Tilføje';
+$LANG['Abstract'] = 'Abstrakt';
+$LANG['No file was specified'] = 'Ingen fil blev angivet';
+$LANG['Edit files'] = 'Rediger filer';
+$LANG['Download'] = 'Hent';
+$LANG['Edit'] = 'Edit';
+$LANG['File was added'] = 'Fil tilføjes';
+$LANG['File was deleted'] = 'Fil blev slettet';
+$LANG['File was edited'] = 'Filen blev redigeret';
